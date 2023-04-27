cbaa3ce6cd3d9df996a7790f674c9473d42a4790
Bobo-San <131928484+Bobo-San@users.noreply.github.com>
2023-04-26 22:01:51 -0400

    Create textgenerator.js

---
+++ textgenerator.js
@@ -0,0 +1,20 @@
+const got = require('got');
+
+async function generate(prompt) {
+  const url = 'https://api.openai.com/v1/engines/davinci/completions';
+  const params = {
+    "prompt": prompt,
+    "max_tokens": 160,
+    "temperature": 0.7,
+    "frequency_penalty": 0.5
+  };
+  const headers = {
+    'Authorization': `Bearer ${process.env.OPENAI_SECRET_KEY}`,
+  };
+
+  const response = await got.post(url, { json: params, headers: headers }).json();
+  output = `${prompt}${response.choices[0].text}`;
+  return output;
+}
+
+module.exports = { generate };
